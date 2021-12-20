42d64bd25876358580f0d2ee7fcd299c8d199b04
Yudai Kobayashi <asuhacoder@gmail.com>
2021-12-20 14:36:40 +0900

    fix: fix bind of test_case #24

--- internal/app/lesson/main.go
+++ internal/app/lesson/main.go
@@ -2,9 +2,7 @@ package lesson
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
-	"fmt"
 	"log"
 	"net"
 
@@ -24,10 +22,38 @@ type server struct {
 	pb.UnimplementedLessonServer
 }
 
+func getCases(lesson db.Lesson) ([]*pb.Case, error) {
+	var pbCases []*pb.Case
+	uUID := lesson.UUID
+	var dbCases []db.TestCase
+	result := db.CaseDB.Find(&dbCases, "LESSON_ID = ?", uUID)
+	if result.Error != nil {
+		log.Printf("failed to get cases: %v", result.Error)
+	}
+	for _, c := range dbCases {
+		pbCases = append(pbCases, &pb.Case{Input: c.INPUT, Output: c.OUTPUT})
+	}
+	return pbCases, result.Error
+}
+
+func getCasesWithID(lesson db.Lesson) ([]*pb.CaseWithID, error) {
+	var pbCases []*pb.CaseWithID
+	uUID := lesson.UUID
+	var dbCases []db.TestCase
+	result := db.CaseDB.Find(&dbCases, "LESSON_ID = ?", uUID)
+	if result.Error != nil {
+		log.Printf("failed to get cases: %v", result.Error)
+	}
+	for _, c := range dbCases {
+		pbCases = append(pbCases, &pb.CaseWithID{LessonId: c.LESSON_ID.String(), Input: c.INPUT, Output: c.OUTPUT})
+	}
+	return pbCases, result.Error
+}
+
 func (s *server) ListLessons(rect *pb.ListLessonsRequest, stream pb.Lesson_ListLessonsServer) error {
 	log.Println("ListLessons running")
 	var lessons []db.Lesson
-	result := db.DB.Find(&lessons)
+	result := db.LessonDB.Find(&lessons)
 	log.Println("got all lessons")
 	log.Println(result.Error)
 	if result.Error != nil {
@@ -36,23 +62,20 @@ func (s *server) ListLessons(rect *pb.ListLessonsRequest, stream pb.Lesson_ListL
 	}
 
 	for _, lesson := range lessons {
-		cases := []*pb.Case{}
-		for i, t := range lesson.TestCase {
-			bytes := []byte(fmt.Sprintf("%v", t))
-			err := json.Unmarshal(bytes, &cases[i])
-			if err != nil {
-				return err
-			}
+		pbCases, err := getCases(lesson)
+		if err != nil {
+			log.Printf("failed to get cases: %v", err)
 		}
 		if err := stream.Send(&pb.ListLessonsReply{
 			Uuid:         lesson.UUID.String(),
 			UserId:       lesson.USER_ID.String(),
+			CourseId:     lesson.COURSE_ID.String(),
 			Title:        lesson.TITLE,
 			Introduction: lesson.INTRODUCTION,
 			Body:         lesson.BODY,
 			DefaultCode:  lesson.DEFAULT_CODE,
 			AnswerCode:   lesson.ANSWER_CODE,
-			TestCase:     cases,
+			TestCase:     pbCases,
 			Language:     lesson.LANGUAGE,
 		}); err != nil {
 			return err
@@ -63,29 +86,26 @@ func (s *server) ListLessons(rect *pb.ListLessonsRequest, stream pb.Lesson_ListL
 
 func (s *server) GetLesson(ctx context.Context, in *pb.GetLessonRequest) (*pb.GetLessonReply, error) {
 	var lesson db.Lesson
-	result := db.DB.First(&lesson, "UUID = ?", in.GetUuid())
+	result := db.LessonDB.First(&lesson, "UUID = ?", in.GetUuid())
 	log.Println(lesson)
 	if result.Error != nil {
 		log.Printf("failed to get a lesson: %v", result.Error)
 		return &pb.GetLessonReply{}, result.Error
 	}
-	cases := []*pb.Case{}
-	for i, t := range lesson.TestCase {
-		bytes := []byte(fmt.Sprintf("%v", t))
-		err := json.Unmarshal(bytes, &cases[i])
-		if err != nil {
-			return &pb.GetLessonReply{}, err
-		}
+	pbCases, err := getCases(lesson)
+	if err != nil {
+		log.Printf("failed to get cases: %v", err)
 	}
 	return &pb.GetLessonReply{
 		Uuid:         lesson.UUID.String(),
 		UserId:       lesson.USER_ID.String(),
+		CourseId:     lesson.COURSE_ID.String(),
 		Title:        lesson.TITLE,
 		Introduction: lesson.INTRODUCTION,
 		Body:         lesson.BODY,
 		DefaultCode:  lesson.DEFAULT_CODE,
 		AnswerCode:   lesson.ANSWER_CODE,
-		TestCase:     cases,
+		TestCase:     pbCases,
 		Language:     lesson.LANGUAGE,
 	}, nil
 }
@@ -101,18 +121,7 @@ func (s *server) CreateLesson(ctx context.Context, in *pb.CreateLessonRequest) (
 	if err != nil {
 		log.Printf("failed to convert string to uuid: %v", err)
 	}
-	dbCases := []db.TestCase{}
-	log.Printf("in.GetTestCase: %v", in.GetTestCase())
-	for _, t := range in.GetTestCase() {
-		bytes := []byte(fmt.Sprintf("%v", t))
-		var dbCase db.TestCase
-		err := json.Unmarshal(bytes, &dbCase)
-		if err != nil {
-			log.Printf("failed to unmarshal[db]: %v", err)
-			return &pb.CreateLessonReply{}, err
-		}
-		dbCases = append(dbCases, dbCase)
-	}
+
 	lesson := db.Lesson{
 		UUID:         uuid.Must(uuid.NewV4()),
 		USER_ID:      uUID,
@@ -122,31 +131,41 @@ func (s *server) CreateLesson(ctx context.Context, in *pb.CreateLessonRequest) (
 		BODY:         in.GetBody(),
 		DEFAULT_CODE: in.GetDefaultCode(),
 		ANSWER_CODE:  in.GetAnswerCode(),
-		TestCase:     dbCases,
 		LANGUAGE:     in.GetLanguage(),
 	}
 	log.Println(lesson)
-	result := db.DB.Create(&lesson)
+	result := db.LessonDB.Create(&lesson)
 	if result.Error != nil {
 		log.Printf("failed to create lesson: %v", result.Error)
 		return &pb.CreateLessonReply{}, result.Error
 	}
 
-	pbCases := []*pb.Case{}
-	for _, t := range lesson.TestCase {
-		bytes := []byte(fmt.Sprintf("%v", t))
-		var pbCase *pb.Case
-		err := json.Unmarshal(bytes, &pbCase)
-		if err != nil {
-			log.Printf("failed to unmarshal[pb]: %v", err)
-			return &pb.CreateLessonReply{}, err
+	log.Printf("in.GetTestCase: %v", in.GetTestCase())
+	for _, t := range in.GetTestCase() {
+		dbCase := db.TestCase{
+			UUID:      uuid.Must(uuid.NewV4()),
+			LESSON_ID: lesson.UUID,
+			INPUT:     t.Input,
+			OUTPUT:    t.Output,
 		}
-		pbCases = append(pbCases, pbCase)
+		result := db.CaseDB.Create(&dbCase)
+		log.Printf("dbCase: %v", dbCase)
+		if result.Error != nil {
+			log.Printf("failed to create cases: %v", result.Error)
+			return &pb.CreateLessonReply{}, result.Error
+		}
 	}
+
+	pbCases, err := getCases(lesson)
+	if err != nil {
+		log.Printf("failed to get cases: %v", err)
+	}
+	log.Printf("pbCases: %v", pbCases)
 
 	return &pb.CreateLessonReply{
 		Uuid:         lesson.UUID.String(),
 		UserId:       lesson.USER_ID.String(),
+		CourseId:     lesson.COURSE_ID.String(),
 		Title:        lesson.TITLE,
 		Introduction: lesson.INTRODUCTION,
 		Body:         lesson.BODY,
@@ -170,7 +189,7 @@ func (s *server) UpdateLesson(ctx context.Context, in *pb.UpdateLessonRequest) (
 	if err != nil {
 		log.Printf("failed to convert string to uuid: %v", err)
 	}
-	result := db.DB.First(&lesson, "UUID = ?", uUID)
+	result := db.LessonDB.First(&lesson, "UUID = ?", uUID)
 	if result.Error != nil {
 		log.Printf("failed to update lesson: %v", result.Error)
 		return &pb.UpdateLessonReply{}, result.Error
@@ -180,35 +199,42 @@ func (s *server) UpdateLesson(ctx context.Context, in *pb.UpdateLessonRequest) (
 		log.Printf("failed to update lesson: %v", err)
 		return &pb.UpdateLessonReply{}, err
 	}
-	dbCases := []db.TestCase{}
-	for i, t := range in.GetTestCase() {
-		bytes := []byte(fmt.Sprintf("%v", t))
-		err := json.Unmarshal(bytes, &dbCases[i])
-		if err != nil {
-			return &pb.UpdateLessonReply{}, err
-		}
-	}
+
 	lesson.TITLE = in.GetTitle()
 	lesson.INTRODUCTION = in.GetIntroduction()
 	lesson.BODY = in.GetBody()
 	lesson.DEFAULT_CODE = in.GetDefaultCode()
 	lesson.ANSWER_CODE = in.GetAnswerCode()
-	lesson.TestCase = dbCases
 	lesson.LANGUAGE = in.GetLanguage()
-	db.DB.Save(&lesson)
+	db.LessonDB.Save(&lesson)
 
-	pbCases := []*pb.Case{}
-	for i, t := range lesson.TestCase {
-		bytes := []byte(fmt.Sprintf("%v", t))
-		err := json.Unmarshal(bytes, &pbCases[i])
-		if err != nil {
-			return &pb.UpdateLessonReply{}, err
-		}
+	var newDbCases []db.TestCase
+	pbCases := in.GetTestCase()
+	for _, c := range pbCases {
+		newDbCases = append(newDbCases, db.TestCase{INPUT: c.Input, OUTPUT: c.Output})
+	}
+
+	var dbCases []db.TestCase
+	result = db.CaseDB.First(&dbCases, "LESSON_ID = ?", uUID)
+	if result.Error != nil {
+		log.Printf("failed to update case: %v", result.Error)
+		return &pb.UpdateLessonReply{}, result.Error
+	}
+	for i, c := range dbCases {
+		c.INPUT = newDbCases[i].INPUT
+		c.OUTPUT = newDbCases[i].OUTPUT
+		db.CaseDB.Save(&c)
+	}
+
+	pbCases, err = getCasesWithID(lesson)
+	if err != nil {
+		log.Printf("failed to get cases: %v", err)
 	}
 
 	return &pb.UpdateLessonReply{
 		Uuid:         lesson.UUID.String(),
 		UserId:       lesson.USER_ID.String(),
+		CourseId:     lesson.COURSE_ID.String(),
 		Title:        lesson.TITLE,
 		Introduction: lesson.INTRODUCTION,
 		Body:         lesson.BODY,
@@ -228,7 +254,7 @@ func (s *server) DeleteLesson(ctx context.Context, in *pb.DeleteLessonRequest) (
 		return new(empty.Empty), err
 	}
 	uUID, err := uuid.FromString(in.GetUuid())
-	result := db.DB.First(&lesson, "UUID = ?", uUID)
+	result := db.LessonDB.First(&lesson, "UUID = ?", uUID)
 	if result.Error != nil {
 		log.Println(err)
 		return new(empty.Empty), result.Error
@@ -236,19 +262,36 @@ func (s *server) DeleteLesson(ctx context.Context, in *pb.DeleteLessonRequest) (
 	if userUuid != lesson.USER_ID {
 		return new(empty.Empty), errors.New("invalid user id")
 	}
-	result = db.DB.Delete(&lesson, "UUID = ?", uUID)
+	var cases []db.TestCase
+	result = db.CaseDB.Find(&cases, "LESSON_ID = ?", lesson.UUID)
+	if result.Error != nil {
+		log.Printf("failed to find cases: %v", result.Error)
+		return new(empty.Empty), result.Error
+	}
+	for _, c := range cases {
+		result = db.CaseDB.Delete(&c, "UUID = ?", c.UUID)
+		if result.Error != nil {
+			log.Printf("failed to delete a case: %v", result.Error)
+			return new(empty.Empty), result.Error
+		}
+	}
+
+	result = db.LessonDB.Delete(&lesson, "UUID = ?", uUID)
 	log.Println(lesson, result.Error)
 	if result.Error != nil {
 		log.Printf("failed to delete a lesson: %v", result.Error)
 		return new(empty.Empty), result.Error
 	}
+
 	return new(empty.Empty), nil
 }
 
 func RunServer() {
 	log.Println("test")
-	db.Init()
-	defer db.Close()
+	db.LessonInit()
+	defer db.LessonClose()
+	db.CaseInit()
+	defer db.CaseClose()
 
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
---
+++ internal/pkg/db/lesson/lesson.go
@@ -0,0 +1,51 @@
+package lesson
+
+import (
+	"log"
+
+	uuid "github.com/gofrs/uuid"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	LessonDB *gorm.DB
+	err      error
+)
+
+func LessonInit() {
+	LessonDB, err = gorm.Open(postgres.New(postgres.Config{
+		DSN: "host=lesson_db user=gorm password=gorm dbname=gorm port=5434 sslmode=disable TimeZone=Asia/Tokyo",
+	}), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	lessonAutoMigration()
+}
+
+func LessonClose() {
+	dbSQL, err := LessonDB.DB()
+	if err != nil {
+		panic(err)
+	}
+	dbSQL.Close()
+}
+
+func lessonAutoMigration() {
+	err := LessonDB.AutoMigrate(&Lesson{})
+	if err != nil {
+		log.Printf("failed to AutoMigrate: %v", err)
+	}
+}
+
+type Lesson struct {
+	UUID         uuid.UUID `gorm:"primaryKey; unique; type:uuid;"`
+	USER_ID      uuid.UUID `gorm:"not null"`
+	COURSE_ID    uuid.UUID `gorm:"not null"`
+	TITLE        string    `gorm:"not null"`
+	INTRODUCTION string    `gorm:"not null"`
+	BODY         string    `gorm:"not null"`
+	DEFAULT_CODE string    `gorm:"not null"`
+	ANSWER_CODE  string    `gorm:"not null"`
+	LANGUAGE     string    `gorm:"not null"`
+}
