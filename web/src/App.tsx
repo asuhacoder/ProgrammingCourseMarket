9f5c465817c736eb5dd1230de2a16090d5387290
Yudai Kobayashi <asuhacoder@gmail.com>
2021-12-27 23:11:37 +0900

    feat: add signup logic #44

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
