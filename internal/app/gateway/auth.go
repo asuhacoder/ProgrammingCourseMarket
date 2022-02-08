45dc0c7c95d6176b3dfd45551a10a5b97a90cd02
Yudai Kobayashi <asuhacoder@gmail.com>
2022-02-08 13:45:20 +0900

    feat: add body binding in auth #44

--- internal/app/gateway/auth.go
+++ internal/app/gateway/auth.go
@@ -14,6 +14,11 @@ const (
 	authAddress = "auth:50052"
 )
 
+type AuthRequest struct {
+	Email    string `form:"email" json:"email"`
+	Password string `form:"password" json:"password"`
+}
+
 func login(c *gin.Context) {
 	conn, err := grpc.Dial(authAddress, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
@@ -22,14 +27,18 @@ func login(c *gin.Context) {
 	defer conn.Close()
 	client := pbAuth.NewAuthClient(conn)
 
-	email := c.Query("email")
-	password := c.Query("password")
+	var s AuthRequest
+	err = c.ShouldBind(&s)
+	if err != nil {
+		log.Printf("failed to bind request: %v", err)
+		c.AbortWithStatus(400)
+	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := client.Login(ctx, &pbAuth.LoginRequest{
-		Email:    email,
-		Password: password,
+		Email:    s.Email,
+		Password: s.Password,
 	})
 	if err != nil {
 		c.AbortWithStatus(400)
@@ -45,5 +54,5 @@ func login(c *gin.Context) {
 
 func authRouters(router *gin.RouterGroup) {
 	a := router.Group("/auth")
-	a.GET("", login)
+	a.POST("", login)
 }
